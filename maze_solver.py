c6675c15f0dffd2e2bab3dd402929764b9655c21
C4triplezero <jcurrie3000@gmail.com>
2024-10-17 02:39:51 -0600

    added maze class with drawing functionality

--- maze_solver.py
+++ maze_solver.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, BOTH, Canvas
+import time
 
 class Window:
     def __init__(self, width, height):
@@ -83,9 +84,61 @@ class Cell:
         self._win.draw_line(line, fill_color)
 
 
+class Maze:
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+        self._cells = []
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+
+        self._create_cells()
+
+    def _create_cells(self):
+        for i in range(self._num_cols):
+            col = []
+            for j in range(self._num_rows):
+                col.append(Cell(self._win))
+            self._cells.append(col)
+        for i in range(len(self._cells)):
+            for j in range(len(self._cells[i])):
+                self._draw_cell(i, j)
+        
+
+
+    def _draw_cell(self, i, j):
+        if self._win == None:
+            return
+        x1 = self._x1 + i * self._cell_size_x
+        x2 = x1 + self._cell_size_x
+        y1 = self._y1 + j * self._cell_size_y
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
+        self._animate()
+
+    def _animate(self):
+        if self._win == None:
+            return
+        self._win.redraw()
+        time.sleep(0.05)
+
+
 
 def main():
-    win = Window(800, 600)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x - 2 * margin) / num_cols
+    cell_size_y = (screen_y - 2 * margin) / num_rows
+    win = Window(screen_x, screen_y)
+
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win)
+
     win.wait_for_close()
 
 
